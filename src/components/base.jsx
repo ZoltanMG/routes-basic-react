25db372d1ba2c5ed5e91f6c53f577f5afffad82b
ZoltanMG <zoltanmoragarcia@gmail.com>
2021-04-21 15:20:07 -0500

    [Add]: validando useEffect

--- src/components/base.jsx
+++ src/components/base.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Base = () => {
+    const [nombre, setNombre] = useState('Zoltán')
+
+    // despues de 2 segundos de la renderizacion completa nombre cambia de 'Zoltan' a 'María'
+    useEffect(() => {
+        setTimeout(() => {
+            setNombre('María')
+        },2000)
+    })
+
     return (
         <div>
             <h1>Pagina de base.</h1>
+            <p>{nombre}</p>
         </div>
     );
 }
